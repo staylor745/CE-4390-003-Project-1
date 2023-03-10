207f78da0b7ca29e47c0fe0692a3654ab850b1f6
Gehrig Wilcox <4042887+gehrigwilcox@users.noreply.github.com>
2023-03-09 19:01:12 -0600

    Create MathHelper.java
    Created helper class to handle math equations

---
+++ src/MathHelper.java
@@ -0,0 +1,24 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+/**
+ * Helper class to process math requests
+ *
+ * Authors: Gehrig Wilcox
+ * */
+public class MathHelper {
+
+    static ScriptEngineManager mgr = new ScriptEngineManager();
+    static ScriptEngine engine = mgr.getEngineByName("JavaScript");
+
+    public static String eval(String eq){
+        try{
+            return (String) engine.eval(eq);
+        }catch(ScriptException e){
+            e.printStackTrace();
+
+            return "";
+        }
+    }
+}
